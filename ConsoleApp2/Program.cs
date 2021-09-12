3474734e4cf53400b03f4f6c7e4c394496ae2bdd
mikes <mikes@DESKTOP-98BQ9FO>
2021-09-12 21:19:03 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Name_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Mike";
+            string lastName;
+            lastName = "Dawson";
+
+            string text = firstName + " " + lastName;
+
+            Console.WriteLine(text);
+        }
+    }
+}
